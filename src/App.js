8049123c834e9f6347e0ae0e071892c91bbf9db4
harishnarasimhan <harishnarasimhan001@gmail.com>
2021-08-22 10:57:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import {useState} from 'react'
+import './App.css';
+
+function App() {
+  let[num,setNum]=useState(0)
+
+  const increment=()=>{
+      setNum(num+1)
+  }
+  const decrement=()=>{
+      setNum(num-1)
+  }
+  return (
+    <div>
+      <div id="main__container">
+        <h1>{num}</h1>
+        <div>
+          <button onClick={increment}>ADD</button>
+          <button onClick={decrement}>SUB</button>
+        </div>  
+      </div>
+    </div>  
+  );
+}
+
+export default App;
